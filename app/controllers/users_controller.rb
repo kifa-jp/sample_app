132288e4bf0a2f20203bef61f811eb0af1bd01f7
kifa-jp <kifa21@gmail.com>
2022-05-11 02:16:49 +0900

    他ユーザーのedit、updateの禁止とフレンドリーフォワーディング対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 # Usersコントローラ
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -37,5 +40,22 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # beforeアクション
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = "Please log in"
+    redirect_to login_url
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
