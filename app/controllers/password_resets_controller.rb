b74f22f14d5808cd3ea96814798cc0a45dc672f5
kifa-jp <kifa21@gmail.com>
2022-05-15 16:12:21 +0900

    パスワード再設定画面と処理を追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :find_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update]
+
   def new; end
 
   def create
@@ -15,4 +19,43 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit; end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render "edit"
+    elsif @user.update(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # beforeフィルタ
+
+  def find_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # 正しいユーザーかどうか確認する
+  def valid_user
+    redirect_to root_url unless @user&.activated? && @user&.authenticated?(:reset, params[:id])
+  end
+
+  # トークンが期限切れかどうか確認する
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = "Password reset has expired."
+    redirect_to new_password_reset_url
+  end
 end
