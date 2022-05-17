f5970a83308cb32b02b8e75fa1df0e5acaaef1f1
kifa-jp <kifa21@gmail.com>
2022-05-17 23:29:50 +0900

    ログイン済みであればhomeにmicropost投稿画面を表示するよう変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 # StaticPagesコントローラ
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @micropost = current_user.microposts.build if logged_in?
+  end
 
   def help; end
 
