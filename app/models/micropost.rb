51c1445ff2a39b9f949841cafe2be857980c2043
kifa-jp <kifa21@gmail.com>
2022-05-19 03:22:45 +0900

    画像投稿機能を作成

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,14 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
+                    size: { less_than: 5.kilobytes, message: "should be less than 5MB" }
+
+  # 表示用のリサイズ済み画像を返す
+  def display_image
+    image.variant(resize_to_limit: [300, 300])
+  end
 end
